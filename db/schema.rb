b20f85b0e6ef53e17b19046da0bed3f18daa56af
Pavlo Vasylkivskyi <pavel.vasylkivskiy@gmail.com>
2021-01-11 07:46:23 +0200

    add user column & association between user & posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_11_051408) do
+ActiveRecord::Schema.define(version: 2021_01_11_052916) do
 
   create_table "posts", force: :cascade do |t|
     t.string "text"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "title"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
